e15257fc0e589af006553375c3498f0ab39e77fc
eddy5641 <eddydavinchi@gmail.com>
2016-02-24 07:00:29 -0800

    Fix icon

---
+++ Sightstone/AppBootstrapper.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Caliburn.Micro;
+using Sightstone.ViewModels;
+using SightStone.Helpers;
+
+namespace SightStone
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        private CompositionContainer _container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            var assemblies = base.SelectAssemblies().ToList();
+            assemblies.Add(typeof(ShellViewModel).GetTypeInfo().Assembly);
+            assemblies.Add(typeof(Sightstone.Views.ShellView).GetTypeInfo().Assembly);
+            return assemblies;
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.SatisfyImportsOnce(instance);
+        }
+
+        /// <summary>
+        ///     By default, we are configured to use MEF
+        /// </summary>
+        protected override void Configure()
+        {
+            //Allow us to load views and viewmodels from different location
+            var config = new TypeMappingConfiguration
+            {
+                DefaultSubNamespaceForViews = "Sightstone.Views",
+                DefaultSubNamespaceForViewModels = "Sightstone.ViewModels"
+            };
+            Caliburn.Micro.ViewLocator.ConfigureTypeMappings(config);
+            ViewModelLocator.ConfigureTypeMappings(config);
+
+            var catalog =
+                new AggregateCatalog(
+                    AssemblySource.Instance.Select(x => new AssemblyCatalog(x)).OfType<ComposablePartCatalog>());
+
+            _container = new CompositionContainer(catalog);
+
+            var batch = new CompositionBatch();
+
+            batch.AddExportedValue<IWindowManager>(new WindowManager());
+            batch.AddExportedValue<IEventAggregator>(new EventAggregator());
+            batch.AddExportedValue(_container);
+            batch.AddExportedValue(catalog);
+
+            _container.Compose(batch);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return _container.GetExportedValues<object>(AttributedModelServices.GetContractName(serviceType));
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            var contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
+            var exports = _container.GetExportedValues<object>(contract);
+
+            var enumerable = exports as object[] ?? exports.ToArray();
+            if (enumerable.Any())
+            {
+                return enumerable.First();
+            }
+
+            throw new Exception($"Could not locate any instances of contract {contract}.");
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            var startupTasks =
+                GetAllInstances(typeof(StartupTask))
+                .Cast<ExportedDelegate>()
+                .Select(exportedDelegate => (StartupTask)exportedDelegate.CreateDelegate(typeof(StartupTask)));
+
+            startupTasks.Apply(s => s());
+            var settings = new Dictionary<string, object>
+{
+    { "Icon", new BitmapImage(new Uri("pack://application:,,,/Sightstone;component/Icon.ico")) },
+    { "ResizeMode", ResizeMode.NoResize }
+};
+
+            DisplayRootViewFor<IShell>(settings);
+        }
+    }
+}
---
+++ agsxmpp/protocol/Base/Stanza.cs
@@ -0,0 +1,91 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2003-2016 by AG-Software 											 *
+ * All Rights Reserved.																 *
+ * Contact information for AG-Software is available at http://www.ag-software.de	 *
+ *																					 *
+ * Licence:																			 *
+ * The agsXMPP SDK is released under a dual licence									 *
+ * agsXMPP can be used under either of two licences									 *
+ * 																					 *
+ * A commercial licence which is probably the most appropriate for commercial 		 *
+ * corporate use and closed source projects. 										 *
+ *																					 *
+ * The GNU Public License (GPL) is probably most appropriate for inclusion in		 *
+ * other open source projects.														 *
+ *																					 *
+ * See README.html for details.														 *
+ *																					 *
+ * For general enquiries visit our website at:										 *
+ * http://www.ag-software.de														 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+namespace agsXMPP.protocol.Base
+{
+	/// <summary>
+	/// Base XMPP Element
+	/// This must ne used to build all other new packets
+	/// </summary>
+	public abstract class Stanza : DirectionalElement
+	{
+		public Stanza()
+		{
+		}
+
+		public Stanza(string tag) : base(tag)
+		{			
+		}
+
+		public Stanza(string tag, string ns) : base(tag)
+		{			
+			Namespace = ns;
+		}
+
+		public Stanza(string tag, string text, string ns) : base(tag, text)
+		{
+			Namespace = ns;
+		}
+
+		public string Id
+		{
+			get 
+			{ 
+				return GetAttribute("id");
+			}
+			set
+			{ 
+				SetAttribute("id", value); 
+			}
+		}
+
+		/// <summary>
+		/// Generates a automatic id for the packet.
+		/// !!! Overwrites existing Ids
+		/// </summary>
+		/// <returns></returns>
+		public void GenerateId()
+		{
+			string sId = agsXMPP.Id.GetNextId();
+			Id = sId;			
+		}
+
+        /// <summary>        
+        /// XML Language attribute
+        /// </summary>
+        /// <remarks>
+        /// The language 'xml:lang' attribute  SHOULD be included by the initiating entity on the header for the initial stream 
+        /// to specify the default language of any human-readable XML character data it sends over that stream. 
+        /// If the attribute is included, the receiving entity SHOULD remember that value as the default for both the 
+        /// initial stream and the response stream; if the attribute is not included, the receiving entity SHOULD use 
+        /// a configurable default value for both streams, which it MUST communicate in the header for the response stream. 
+        /// For all stanzas sent over the initial stream, if the initiating entity does not include an 'xml:lang' attribute, 
+        /// the receiving entity SHOULD apply the default value; if the initiating entity does include an 'xml:lang' attribute, 
+        /// the receiving entity MUST NOT modify or delete it (see also xml:langxml:lang). 
+        /// The value of the 'xml:lang' attribute MUST conform to the format defined in RFC 3066 (Tags for the Identification of Languages, January 2001.[LANGTAGS]).
+        /// </remarks>
+        public string Language
+        {
+            get { return GetAttribute("xml:lang"); }
+            set { SetAttribute("xml:lang", value); }
+        }
+	}
+}
