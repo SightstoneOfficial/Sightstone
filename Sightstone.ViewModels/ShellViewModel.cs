5a5aae70668d41b23e485438826295b7313cca80
eddy5641 <eddydavinchi@gmail.com>
2016-02-25 22:58:00 -0800

    Some ui changes, now you can close the window

--- Sightstone.ViewModels/ShellViewModel.cs
+++ Sightstone.ViewModels/ShellViewModel.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.ComponentModel.Composition;
+using System.Windows;
 using Caliburn.Micro;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Sightstone.Views;
 
 namespace Sightstone.ViewModels
 {
     [Export(typeof(IShell))]
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<ShellView>
     {
-        public override void CanClose(Action<bool> callback)
+        [ImportingConstructor]
+        public ShellViewModel(IWindowManager windowManager)
         {
             
+        }
+        public async override void CanClose(Action<bool> callback)
+        {
+            var progressClose = await (Application.Current.MainWindow as MetroWindow).ShowMessageAsync("Quit", "Are you sure you want to quit?", MessageDialogStyle.AffirmativeAndNegative);
+            if (progressClose == MessageDialogResult.Affirmative)
+                callback(true);
             callback(false);
         }
         public void Close()
